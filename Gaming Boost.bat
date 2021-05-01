9fca4ed5727e0d01ff02cf88bd8d270b437c74fa
Philip-Otter <philip.jj.otter@gmail.com>
2021-05-01 16:43:59 -0400

    Added new tasks to kill for Gaming Boost:
    Spotify
    Chrome
    MS Edge
    MS Word

--- Gaming Boost.bat
+++ Gaming Boost.bat
@@ -9,9 +9,17 @@ taskkill /F /IM Vive.exe
 taskkill /F /IM ViveSettings.exe
 :: This should eventually ask if you are using a vpn
 taskkill /F /IM NordVPN.exe
+:: This should eventually ask if you are listening to external music
+taskkill /F /IM Spotify.exe
+:: This should eventually ask if you are using web browsers
+taskkill /F /IM msedge.exe
+taskkill /F /IM chrome.exe
+:: This should ask if you are using any document services
+taskkill /F /IM WINWORD.EXE
 :: There should be no asking for these
 taskkill /F /IM Wacom_TabletUser.exe
 taskkill /F /IM YourPhone.exe
 taskkill /F /IM wallpaper64.exe
 taskkill /F /IM Cortana.exe
 taskkill /F /IM Skype.exe
+
