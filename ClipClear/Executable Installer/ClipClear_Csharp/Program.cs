e96d9105b1be8d8f180b8dfeb4c11df327831b3f
Philip-Otter <philip.jj.otter@gmail.com>
2021-05-20 20:11:14 -0400

    First Commit of ClipClear C# Build
    Fixed:
    
    	NONE
    
    Added:
    
    	ClipClear written in C# and all of it's dependancies.
    
    	ClipClear Installer and all of its dependancies.
    
    Modified:
    
    	NONE

---
+++ ClipClear/Executable Installer/ClipClear_Csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿/* ClipClear written in the C# language
+ * 
+ * Philip Otter, last edit:  05/20/2021 */
+
+using System;
+
+namespace ClipClear_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Runs an infinite loop that clears the clipboard.
+            while (true)
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C @echo off | clip";
+                process.StartInfo = startInfo;
+                _ = process.Start();
+                // Prevents the clipboard from being cleared for 10 minutes.
+                System.Threading.Thread.Sleep(600000);
+            }
+            
+        }
+    }
+}
